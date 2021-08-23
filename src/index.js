0c3af8e8fd10654fd75aae4a6969e4077e4b034b
Gimna Katugampala <autoservicelk@gmail.com>
2021-08-23 00:33:15 -0700

    correction in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import reactDom from 'react-dom';
+import ReactDom from 'react-dom';
 import App from './App'
 
-ReactDOM.render(<App/> ,document.getElementById('root'))
+ReactDom.render(<App/> ,document.getElementById('root'))
