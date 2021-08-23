22aadee4299fd4db94de149ab507b5f73799d76c
Gimna Katugampala <autoservicelk@gmail.com>
2021-08-23 13:21:28 -0700

    login form created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import {ChatEngine} from 'react-chat-engine'
 import ChatFeed from './components/ChatFeed'
+import LoginForm from './components/LoginForm'
 
 import  './App.css'
 
 const App = () => {
+
+    if(!localStorage.getItem('username')) return <LoginForm/>
+
     return (
        
        <ChatEngine height="200vh" projectID="
-       0bbc5050-9891-44bb-a021-c9357ad1c236" userName="John" userSecret="123" renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />} />
+       0bbc5050-9891-44bb-a021-c9357ad1c236" userName={localStorage.getItem('username')} userSecret={localStorage.getItem('password')}  renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />} />
     )
 }
 
