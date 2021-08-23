91fe41addee1b9710cc3faca2094a8c8b4060421
Gimna Katugampala <autoservicelk@gmail.com>
2021-08-23 12:43:27 -0700

    basics of chatfeed, mymessages, messageform, thiermessage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react'
 import {ChatEngine} from 'react-chat-engine'
+import ChatFeed from './components/ChatFeed'
+
 import  './App.css'
 
 const App = () => {
     return (
-        <div>
-            Hello World!
-        </div>
+       
+       <ChatEngine height="200vh" projectID="
+       0bbc5050-9891-44bb-a021-c9357ad1c236" userName="Gimna" userSecret="123" renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />} />
     )
 }
 
